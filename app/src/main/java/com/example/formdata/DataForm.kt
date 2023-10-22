ff71beb64b316eea861fb745b1b468bea6e46761
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-10-22 14:41:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/formdata/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.formdata
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/formdata/DataSource.kt
@@ -0,0 +1,8 @@
+package com.example.formdata
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.perempuan
+    )
+}
