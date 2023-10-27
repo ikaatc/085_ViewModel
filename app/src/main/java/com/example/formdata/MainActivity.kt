ccc4b3f7fe7bb178fc87b50ac83552eb3f0fd23d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-10-27 13:43:46 +0700

    menambahkan icon arrow back, divider, dan text

--- app/src/main/java/com/example/formdata/MainActivity.kt
+++ app/src/main/java/com/example/formdata/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.formdata
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -33,7 +35,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -70,6 +74,20 @@ fun TampilLayout(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(20.dp)
         ) {
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+                    contentDescription = "",
+                    modifier = Modifier
+                )
+                Text(text = "Register")
+            }
+            Divider()
+            Text(
+                text = "Create Your Account",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
             TampilForm()
         }
 
