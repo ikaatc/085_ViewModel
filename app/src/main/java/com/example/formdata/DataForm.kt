bde095dc949186534b7ef257deaf4b8916e258a2
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-10-27 13:54:25 +0700

    menambahkan val di DataSource dan DataForm

--- app/src/main/java/com/example/formdata/DataForm.kt
+++ app/src/main/java/com/example/formdata/DataForm.kt
@@ -1,5 +1,6 @@
 package com.example.formdata
 
 data class DataForm(
-    val sex: String = ""
+    val sex: String = "",
+    val stat: String = ""
 )
--- app/src/main/java/com/example/formdata/DataSource.kt
+++ app/src/main/java/com/example/formdata/DataSource.kt
@@ -5,4 +5,9 @@ object DataSource {
         R.string.laki,
         R.string.perempuan
     )
+
+    val status = listOf(
+        R.string.belum,
+        R.string.menikah
+    )
 }
